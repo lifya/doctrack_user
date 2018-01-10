6fb0f040641adb26db44a509dcf3ec4c1512ccbe
lifya <lifya fitriani@gmail.com>
2018-01-10 12:18:51 +0700

    push

---
+++ app/src/main/java/com/pln/www/Detail_Proses_Activity.java
@@ -0,0 +1,13 @@
+package com.pln.www;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_Proses_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__proses_);
+    }
+}
