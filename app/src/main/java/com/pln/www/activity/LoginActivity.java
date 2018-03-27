35375df05314eb4ec82f1518402d82c605969be8
apriliaaachi <achiaprilia.aa@gmail.com>
2018-03-27 09:17:32 +0700

    push finish

--- app/src/main/java/com/pln/www/activity/LoginActivity.java
+++ app/src/main/java/com/pln/www/activity/LoginActivity.java
@@ -17,6 +17,11 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pln.www.R;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
@@ -27,6 +32,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private EditText etUsername, etPassword;
     private ProgressDialog progressDialog;
     private TextView fpass;
+    private DatabaseReference dbUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etUsername = (EditText) findViewById(R.id.etUsername);
         etPassword = (EditText) findViewById(R.id.etPassword);
         bLogin = (Button) findViewById(R.id.bLogin);
+
+        dbUsers = FirebaseDatabase.getInstance().getReference("Users");
         bLogin.setOnClickListener(this);
       //  progressDialog = new ProgressDialog(this);
 
@@ -71,10 +79,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
 
     private void userLogin(){
-        final String username = etUsername.getText().toString().trim();
+        final String email = etUsername.getText().toString().trim();
         final String password = etPassword.getText().toString().trim();
 
-        if(TextUtils.isEmpty(username)){
+        if(TextUtils.isEmpty(email)){
             Toast.makeText(LoginActivity.this, "Please Enter Email", Toast.LENGTH_LONG).show();
             return;
         }
@@ -84,24 +92,59 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             return;
         }
 
-        onWait();
+        progressDialog.setMessage("Please wait");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
 
-        mAuth.signInWithEmailAndPassword(username, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressDialog.dismiss();
-                        if(task.isSuccessful()){
-                            progressDialog.dismiss();
-                            startActivity(new Intent(getApplicationContext(), BerandaActivity.class));
+        dbUsers.orderByChild("email").equalTo(email).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot child : dataSnapshot.getChildren()){
+                    String idUser = child.getKey();
+                    System.out.println(idUser);
+                    dbUsers.child(idUser).child("status").addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            int status = Integer.parseInt(dataSnapshot.getValue().toString());
+                            if(status == 1){
+                                mAuth.signInWithEmailAndPassword(email, password)
+                                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                progressDialog.dismiss();
+                                                if(task.isSuccessful()){
+                                                    finish();
+                                                    startActivity(new Intent(getApplicationContext(), BerandaActivity.class));
+                                                }
+                                                else{
+                                                    Toast.makeText(LoginActivity.this, "Failed ! Check Your Email or Password", Toast.LENGTH_LONG).show();
+                                                    return;
+                                                }
+                                            }
+                                        });
+                            }
+                            else{
+                                progressDialog.dismiss();
+                                Toast.makeText(LoginActivity.this, "Failed ! Unregistered", Toast.LENGTH_LONG).show();
+                                return;
+                            }
                         }
-                        else{
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
                             progressDialog.dismiss();
-                            Toast.makeText(LoginActivity.this, "Failed ! Check Your Email or Password", Toast.LENGTH_LONG).show();
+                            Toast.makeText(LoginActivity.this, "Error !", Toast.LENGTH_LONG).show();
                             return;
                         }
-                    }
-                });
+                    });
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                progressDialog.dismiss();
+                Toast.makeText(LoginActivity.this, "Error !", Toast.LENGTH_LONG).show();
+                return;
+            }
+        });
     }
 
     @Override
